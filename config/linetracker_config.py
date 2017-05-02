af646bcb636276c91311388b00c1cb8df6acb357
Brady Latsha <brady.latsha@gmail.com>
2017-05-01 23:46:42 -0400

    initial commit

---
+++ config/linetracker_config.py
@@ -0,0 +1,28 @@
+import os
+
+# region Scoresandodds API config
+# base json api url for scoresandodds
+feed_url = "http://www.scoresandodds.com/feeds/day/"
+# endregion
+
+# region Database config
+db_file_name = "linetracker.db"
+db_file = os.path.abspath(os.path.join(__file__, "..", "..", "database", db_file_name))
+# endregion
+
+# region SQL queries
+query = {'day': {}}
+query['day']['insert'] = """
+INSERT INTO da y (day_id, date, lastmodified) VALUES (?, ?, ?)
+"""
+query['day']['check_day_exists'] = """
+SELECT * FROM day WHERE date = ?
+"""
+query['day']['update_lastmodified'] = """
+UPDATE day SET lastmodified = ? WHERE date = ?
+"""
+# endregion
+
+# region League config
+selected_leagues = ['NBA', 'MLB', 'NHL']
+# endregion
---
+++ main.py
@@ -0,0 +1,92 @@
+import requests
+import json
+from datetime import datetime
+from config.linetracker_config import db_file, query, feed_url
+import sqlite3
+import uuid
+import logging
+import par
+
+logging.basicConfig(level=logging.DEBUG)
+_logger = logging.getLogger('main')
+# Set as DEBUG for testing
+
+# Database connection global
+db_conn = None
+
+# region Dictionary factory for sqlite queries
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+# endregion
+
+# region Database connection methods
+def opendb():
+    global db_conn
+    _logger.debug("Opening database connection")
+    try:
+       db_conn = sqlite3.connect(db_file)
+       db_conn.row_factory = dict_factory
+    except Exception as e:
+        _logger.debug("Could not open a connection to the database: " + str(e))
+    return db_conn.cursor()
+
+def closedb():
+    global db_conn
+    db_conn.close()
+# endregion
+
+def update_day():
+    c = opendb()
+    today = datetime.today().strftime('%Y-%m-%d')
+    try:
+        _logger.info("Getting feed data for: " + str(today))
+        game_data = get_feed_data()
+    except Exception as e:
+        _logger.debug("Failed to get feed data: " + str(e))
+    day_id = str(uuid.uuid4()).replace("-", "")
+
+# region Check if day already exists
+    c.execute(query['day']['check_day_exists'], (today,))
+    day_data = c.fetchall()
+    if len(day_data) < 1:
+        feed_data_modified = False
+        _logger.info("Day does not exist, inserting new day: " + today)
+        try:
+            values = (day_id, game_data['day']['dateandtime'], game_data['day']['lastmodified'])
+            c.execute(query['day']['insert'], values)
+            db_conn.commit()
+        except Exception as e:
+            _logger.debug("Failed to insert new day: " + str(e))
+    elif int(game_data['day']['lastmodified']) != int(day_data[0]['lastmodified']):
+        feed_data_modified = True
+        new_lastmodified = int(game_data['day']['lastmodified'])
+        old_lastmodified = int(day_data[0]['lastmodified'])
+        _logger.info("Last modified date has changed. Was: " + str(old_lastmodified) + ", Now: " +
+                     str(new_lastmodified) + " updating row in database for day: " + str(today))
+        lastmodified = int(game_data['day']['lastmodified'])
+        c.execute(query['day']['update_lastmodified'], (lastmodified, today,))
+        db_conn.commit()
+    else:
+        _logger.info("Game data has not changed since last update")
+        feed_data_modified = False
+    # endregion
+
+    # close the database connection
+    closedb()
+
+
+def get_feed_data():
+    today = datetime.today().strftime('%Y%m%d')
+    response = requests.get(feed_url + today)
+    json_data = json.loads(response.text.lower())
+    return json_data
+
+def main():
+    update_day()
+
+
+if __name__ == "__main__":
+    main()
